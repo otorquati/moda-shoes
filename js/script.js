c3aedfbf77f7de1230e2fb7627b4a4c041fa0d92
otorquati <torquati.osvaldo@gmail.com>
2024-02-07 18:04:11 -0300

    feat: deploy card generate dinamically

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('Moda Shoes');
+console.log('Moda Shoes')
+
+const getProducts = async () => { 
+    const response = await fetch('js/products.json')
+    const data = await response.json()
+    // console.log(data)
+    return data
+}
+
+const generateCard = async () => {
+    const products = await getProducts()
+    
+    products.map(product => {
+        //console.log(product)
+        let card = document.createElement('div')
+        card.classList.add('card__produto')
+        card.innerHTML = ` <figure>
+        <img src="img/${product.image}" alt=${product.product_name} />
+    </figure>
+    <div class="card__produto_detalhes">
+        <h4>${product.product_name}</h4>
+        <h5>Modelo:${product.product_model}</h5>
+    </div>
+    <p class="card__produto_preco">Preço: R$ ${product.price}</h6>`
+    const listaProdutos = document.querySelector('.lista__produtos')
+    listaProdutos.appendChild(card)
+    })
+}
+
+generateCard()
+
